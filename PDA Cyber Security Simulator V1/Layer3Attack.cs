0749c0e6dfc38167aefc1cd46a50460cb3c3182f
collapse157 <p3collapse@gmail.com>
2018-01-18 16:52:08 -0500

    Fixed Layer3Attack

---
+++ PDA Cyber Security Simulator V1/Layer3Attack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDA_Cyber_Security_Simulator_V1
+{
+    public class Layer3Attack : Attack
+    {
+        public Network selectedNetwork { get; set; }
+
+        public void startAttack()
+        {
+
+        }
+
+        public void stopAttack()
+        {
+
+        }
+
+        public void computeStats()
+        {
+
+        }
+
+        public Layer3Attack()
+        {
+            selectedNetwork = null;
+        }
+
+    }
+}
