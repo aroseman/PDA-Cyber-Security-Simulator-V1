7757edf6fe58f96b653bf0d24d6f5601f771db1c
Andrew Roseman <whoisroseman@gmail.com>
2018-01-26 19:22:46 -0500

    Added Node and Edge class in preperation for Network builder.

---
+++ PDA Cyber Security Simulator V1/Edge.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDA_Cyber_Security_Simulator_V1
+{
+    class Edge
+    {
+    }
+}
---
+++ PDA Cyber Security Simulator V1/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDA_Cyber_Security_Simulator_V1
+{
+    class Node
+    {
+        System.Drawing.Point location;
+    }
+}
