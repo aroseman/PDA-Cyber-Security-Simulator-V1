ff3ae5b9804157c67cba9fb83ca9a61ac0c9914d
collapse2017 <p3collapse@gmail.com>
2018-01-28 17:33:32 -0500

    FIXED Added SQLite with Device table.

--- PDA Cyber Security Simulator V1/Device.cs
+++ PDA Cyber Security Simulator V1/Device.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Data.SQLite;
+using System.Diagnostics;
 using PDA_Cyber_Security_Simulator_V1;
 
 public class Device
 {
-    public string Name { get; set; }
+    public String Name { get; set; }
 
-    public string IpAddress { get; set; }
+    public String IpAddress { get; set; }
    
-    public string MacAddress { get; set; }
+    public String MacAddress { get; set; }
 
-    public string Description { get; set; }
+    public String Description { get; set; }
 
     //1 is connected, 0 is disconnected
     public bool Status { get; set; }
@@ -19,16 +20,53 @@ public class Device
     //Contains Neighboring IP Addresses
     public List<Device> Neighbors { get; set; }
 
-    public string Notes { get; set; }
+    public String Notes { get; set; }
 
     public Device()
     {
-        Name = string.Empty;
-        IpAddress = string.Empty;
-        MacAddress = string.Empty;
-        Description = string.Empty;
+        Name = "";
+        IpAddress = "";
+        MacAddress = "";
+        Description = "";
         Status = false;
         Neighbors = new List<Device>();
-        Notes = string.Empty;
+        Notes = "";
+    }
+
+    public static void addDevice(Device newDevice)
+    {
+        SQLiteConnection dbConnection = new SQLiteConnection("Data Source=db.sqlite;Version=3;");
+        dbConnection.Open();
+
+        SQLiteCommand insertDevice = dbConnection.CreateCommand();
+        insertDevice.CommandText = "INSERT INTO device (ip, name) VALUES ('192.168.1.5' , 'Cisco Router');";
+        insertDevice.ExecuteNonQuery();
+    }
+
+    public static String getDevices()
+    {
+        SQLiteConnection dbConnection = new SQLiteConnection("Data Source=db.sqlite;Version=3;");
+        dbConnection.Open();
+        
+        SQLiteCommand getDevice = dbConnection.CreateCommand();
+        getDevice.CommandText = "SELECT * FROM device";
+        SQLiteDataReader deviceReader = getDevice.ExecuteReader();
+
+        deviceReader.Read();
+        deviceReader.Read();
+        String ip = deviceReader.GetString(0);
+        String name = deviceReader.GetString(1);
+
+        return ip + name;
+    }
+
+    public static void makeDeviceTable()
+    {
+        SQLiteConnection dbConnection = new SQLiteConnection("Data Source=db.sqlite;Version=3;");
+        dbConnection.Open();
+
+        SQLiteCommand createDeviceTable = dbConnection.CreateCommand();
+        createDeviceTable.CommandText = "CREATE TABLE device (ip varchar(15) primary key, name varchar(50));";
+        createDeviceTable.ExecuteNonQuery();
     }
 }
---
+++ PDA Cyber Security Simulator V1Tests/DeviceTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PDA_Cyber_Security_Simulator_V1;
+
+namespace PDA_Cyber_Security_Simulator_V1Tests
+{
+    [TestClass]
+    public class DeviceTest
+    {
+        [TestMethod]
+        public void addDeviceTest()
+        {
+            Device testDevice = new Device();
+            testDevice.Name = "CiscoRouter";
+            testDevice.IpAddress = "192.168.1.5";
+
+            Device.addDevice(testDevice);
+        }
+
+        [TestMethod]
+        public void getDevicesTest()
+        {
+            String devices = Device.getDevices();
+
+            Debug.WriteLine(devices);
+        }
+    }
+}
