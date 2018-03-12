0f21a83cc84d4bf730c0ad5683615af2eb9f281b
collapse2017 <p3collapse@gmail.com>
2018-03-12 09:56:29 -0400

    SaveNetwork is now in the presenter.

--- PDA Cyber Security Simulator V1/NetBuilderInterface.cs
+++ PDA Cyber Security Simulator V1/NetBuilderInterface.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,5 +22,13 @@ namespace PDA_Cyber_Security_Simulator_V1
 
         HomeView Form1 { get; }
         HomeViewPresenter Form1Presenter { get; }
+
+        List<Point> Pt1 { get; }
+        List<Point> Pt2 { get; }
+        List<PictureBox> ActiveDevices { get; }
+        Panel Canvas { get; }
+
+        String NetName { get; }
+        Network Network { get; }
     }
 }
