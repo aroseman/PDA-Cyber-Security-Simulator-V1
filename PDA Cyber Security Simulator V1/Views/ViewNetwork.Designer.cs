2d329ecf11bc292e3e4d5629edb46759028dd362
Unknown <mgj5046@psu.edu>
2018-03-21 12:00:50 -0400

    Beginning View Network
    Created Interface and Presenter

---
+++ PDA Cyber Security Simulator V1/Views/ViewNetwork.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace PDA_Cyber_Security_Simulator_V1
+{
+    partial class ViewNetwork
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadNetwork = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoadNetwork
+            // 
+            this.btnLoadNetwork.Location = new System.Drawing.Point(698, 327);
+            this.btnLoadNetwork.Name = "btnLoadNetwork";
+            this.btnLoadNetwork.Size = new System.Drawing.Size(158, 53);
+            this.btnLoadNetwork.TabIndex = 0;
+            this.btnLoadNetwork.Text = "Load Network";
+            this.btnLoadNetwork.UseVisualStyleBackColor = true;
+            // 
+            // ViewNetwork
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2164, 1062);
+            this.Controls.Add(this.btnLoadNetwork);
+            this.Name = "ViewNetwork";
+            this.Text = "7";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadNetwork;
+    }
+}
---
+++ PDA Cyber Security Simulator V1/Views/ViewNetwork.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace PDA_Cyber_Security_Simulator_V1
+{
+    public partial class ViewNetwork : Form
+    {
+        public ViewNetwork()
+        {
+            InitializeComponent();
+        }
+    }
+}
