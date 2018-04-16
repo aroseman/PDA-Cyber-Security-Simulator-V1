76823e8682f15134344646682906b395079d4f1d
Matt <mgj5046@psu.edu>
2018-04-15 22:12:22 -0400

    Added a Push and Pull Entire Network Test

--- PDA Cyber Security Simulator V1Tests/NetworkTest.cs
+++ PDA Cyber Security Simulator V1Tests/NetworkTest.cs
@@ -21,6 +21,9 @@ namespace PDA_Cyber_Security_Simulator_V1Tests
 
             unitOfWork.NetworkManager.DropTable();
             unitOfWork.NetworkManager.CreateTable();
+
+            unitOfWork.NeighborManager.DropNeighborsTable();
+            unitOfWork.NeighborManager.MakeNeighborsTable();
         }
 
         [TestMethod()]
@@ -57,6 +60,129 @@ namespace PDA_Cyber_Security_Simulator_V1Tests
             Assert.AreEqual(2, devices.Count);
         }
 
-        
+        [TestMethod()]
+        public void PushAndPullEntireNetworkTest()
+        {
+            unitOfWork.NetworkManager.AddNetwork(new Network("TestNetwork"));
+            var id = unitOfWork.NetworkManager.GetNetworkIdByName("TestNetwork");
+
+            var device1 = new Device()
+            {
+                Name = "dev1",
+                Configured = false,
+                Description = "This is a test",
+                Id = default(int),
+                IpAddress = "1.1.1.1",
+                MacAddress = "00-00-00-00-00-00",
+                Neighbors = new List<Device>(),
+                NetworkId = id,
+                Notes = "This is a test",
+                Status = false
+            };
+
+            var device2 = new Device()
+            {
+                Name = "dev2",
+                Configured = false,
+                Description = "This is a test",
+                Id = default(int),
+                IpAddress = "2.2.2.2",
+                MacAddress = "00-00-00-00-00-01",
+                Neighbors = new List<Device>(),
+                NetworkId = id,
+                Notes = "This is a test",
+                Status = false
+            };
+
+            var device3 = new Device()
+            {
+                Name = "dev3",
+                Configured = false,
+                Description = "This is a test",
+                Id = default(int),
+                IpAddress = "3.3.3.3",
+                MacAddress = "00-00-00-00-00-02",
+                Neighbors = new List<Device>(),
+                NetworkId = id,
+                Notes = "This is a test",
+                Status = false
+            };
+
+            unitOfWork.DeviceManager.AddDevice(device1);
+            unitOfWork.DeviceManager.AddDevice(device2);
+            unitOfWork.DeviceManager.AddDevice(device3);
+            device1.Id = unitOfWork.DeviceManager.GetDeviceIdByNameAndNetworkId(device1.Name, id);
+            device2.Id = unitOfWork.DeviceManager.GetDeviceIdByNameAndNetworkId(device2.Name, id);
+            device3.Id = unitOfWork.DeviceManager.GetDeviceIdByNameAndNetworkId(device3.Name, id);
+
+            device1.Neighbors.Add(device2);
+            device1.Neighbors.Add(device3);
+            device2.Neighbors.Add(device1);
+            device2.Neighbors.Add(device3);
+            device3.Neighbors.Add(device1);
+            device3.Neighbors.Add(device2);
+
+            foreach (var neighbor in device1.Neighbors)
+            {
+                unitOfWork.NeighborManager.AddNeighbor(device1.Id, neighbor.Id);
+            }
+
+            foreach (var neighbor in device2.Neighbors)
+            {
+                unitOfWork.NeighborManager.AddNeighbor(device2.Id, neighbor.Id);
+            }
+
+            foreach (var neighbor in device3.Neighbors)
+            {
+                unitOfWork.NeighborManager.AddNeighbor(device3.Id, neighbor.Id);
+            }
+
+            var network = unitOfWork.NetworkManager.GetNetworkByName("TestNetwork");
+            var devices = unitOfWork.NetworkManager.GetDeviceByNetworkId(network.Id);
+
+            foreach (var device in devices)
+            {
+                var neighbors = unitOfWork.NeighborManager.GetNeighbors(device.Id);
+
+                foreach (var neighbor in neighbors)
+                {
+                    var neighborDevice = unitOfWork.DeviceManager.GetDeviceById(neighbor.NeighborId);
+                    device.Neighbors.Add(neighborDevice);
+                }
+            }
+
+            var neighborCheckList = true;
+            var neighborCheck1 = false;
+            var neighborCheck2 = false;
+            var neighborCheck3 = false;
+
+            foreach (var device in devices)
+            {
+                if (device.Neighbors.Count != 2)
+                {
+                    neighborCheckList = false;
+                    break;
+                }
+
+                foreach (var neighbor in device.Neighbors)
+                {
+                    if (neighbor.Name == device1.Name)
+                        neighborCheck1 = true;
+                    else if (neighbor.Name == device2.Name)
+                        neighborCheck2 = true;
+                    else if (neighbor.Name == device3.Name)
+                        neighborCheck3 = true;
+                    else
+                    {
+                        neighborCheck1 = false;
+                        neighborCheck2 = false;
+                        neighborCheck3 = false;
+                        break;
+                    }
+                }
+            }
+
+            Assert.IsTrue(neighborCheckList && neighborCheck1 && neighborCheck2 && neighborCheck3);
+        }
     }
 }
