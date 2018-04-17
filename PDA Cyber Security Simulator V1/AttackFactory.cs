1b19bb230b6946cf6548e6415e5764b5d1228b88
collapse2017 <p3collapse@gmail.com>
2018-04-17 09:24:04 -0400

    Added attack test with factory.

--- PDA Cyber Security Simulator V1/AttackFactory.cs
+++ PDA Cyber Security Simulator V1/AttackFactory.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace PDA_Cyber_Security_Simulator_V1
 {
-    class AttackFactory
+    public class AttackFactory
     {
         public AttackFactory()
         {
---
+++ PDA Cyber Security Simulator V1Tests/AttackTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PDA_Cyber_Security_Simulator_V1;
+
+namespace PDA_Cyber_Security_Simulator_V1Tests
+{
+    [TestClass]
+    public class AttackTest
+    {
+        AttackFactory factory = new AttackFactory();
+
+        [TestMethod()]
+        public void SynFloodTest()
+        {
+            IAttack attack = factory.GetAttack("SynFlood");
+            attack.AttackDevice("192.168.1.200", "00-00-00-00-00-00");
+        }
+
+        [TestMethod()]
+        public void ArpFloodTest()
+        {
+            IAttack attack = factory.GetAttack("ArpFlood");
+            attack.AttackDevice("192.168.1.200", "00-00-00-00-00-00");
+        }
+
+        [TestMethod()]
+        public void MacFloodTest()
+        {
+            IAttack attack = factory.GetAttack("MacFlood");
+            attack.AttackDevice("192.168.1.200", "00-00-00-00-00-00");
+        }
+
+    }
+}
